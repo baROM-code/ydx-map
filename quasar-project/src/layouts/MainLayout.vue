d7edcf42fb43cf900a256cd7c14b94b9110c2a5b
ba-ROM-code <barom64@mail.ru>
2022-11-19 19:03:49 +0400

    first

---
+++ quasar-project/src/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-layout>
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Quasar App - Yandex maps with weatehr
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+
+<script>
+   //src="https://api-maps.yandex.ru/2.1/?apikey=90a67968-ccd8-4782-88e4-8fb2f21ba155&lang=ru_RU"
+</script>
---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page>
+    <div class="p-4">
+      <p>Map</p>
+      <div id="map" style="width: 800px; height: 600px"></div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+
+
+
+</script>
+
