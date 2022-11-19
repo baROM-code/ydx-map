15a8510bf3526fbff4ce73c43d8c38a3811b7085
ba-ROM-code <barom64@mail.ru>
2022-11-20 01:39:50 +0400

    Рабочая версия

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -15,5 +15,7 @@
 </template>
 
 
-<script src="https://api-maps.yandex.ru/2.1/?apikey=90a67968-ccd8-4782-88e4-8fb2f21ba155&lang=ru_RU">
+<script>
+
+
 </script>
--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,7 +1,6 @@
 <template>
   <q-page>
     <div class="p-4">
-      <p>Map</p>
       <div id="map" style="width: 800px; height: 600px"></div>
     </div>
   </q-page>
@@ -11,12 +10,40 @@
 
 ymaps.ready(init);
 function init(){
+  coords = [51.547, 46.025];
   // Создание карты.
   var myMap = new ymaps.Map("map", {
     // Порядок по умолчанию: «широта, долгота».
-    center: [55.76, 37.64],
-    zoom: 7
+    center: coords,
+    zoom: 10,
+    controls: ["fullscreenControl", "searchControl" , "zoomControl"]
   });
+  var myPlacemark = new ymaps.Placemark(coords,{ hintContent: "Нажмите для получения прогноза погоды"}, {draggable: "true"});
+
+  myMap.events.add('click', function (e) {
+    coords = e.get('coords');
+    // Перемещение точки по клику
+    myPlacemark.geometry.setCoordinates(coords);
+
+    /*
+    // Вывод сведений о погоде по клику
+    myMap.balloon.open(coords, {
+        contentBody: 'Погода:<br><img src="http://www.7timer.info/bin/astro.php?lon=' + coords[1] + '&lat=' + coords[0] + '&ac=0&lang=en&unit=metric&output=internal&tzshift=0">'
+      }
+    );
+     */
+
+  });
+
+  myPlacemark.events.add(['click', 'dragend'], function (e) {
+    coords = e.get('coords');
+    content = 'Погода в: <br>' +
+              '<img src="http://www.7timer.info/bin/astro.php?lon=' + coords[1] + '&lat=' + coords[0] + '&ac=0&lang=en&unit=metric&output=internal&tzshift=0">';
+    myPlacemark.properties.set('balloonContent', content);
+  });
+
+  myMap.geoObjects.add(myPlacemark);
 }
+
 </script>
 
